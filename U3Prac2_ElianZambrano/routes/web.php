4c5e6d21fba19051e33944a3244259b1479197ee
Elian-Zmb <elianzambrano470@gmail.com>
2026-02-04 18:45:15 -0500

    Delete U3Prac2_ElianZambrano directory

--- U3Prac2_ElianZambrano/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    '/' => 'app/controller/home.php',
-    '/about' => 'app/controller/about.php',
-    '/links' => 'app/controller/links.php',
-    '/post' => 'app/controller/post.php',
-    '/links/create' => 'app/controller/links-create.php'
-];
